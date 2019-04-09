15c745647bef64757a1a23d4d1bd215c405e31fe
Ragnar Stølsmark <ragnar@identitystream.com>
2019-04-09 16:51:49 +0200

    Initial commit

---
+++ PasswordAuthenticationMiddleware.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Rstolsmark.Owin.PasswordAuthentication {
+
+	using AppFunc = Func<IDictionary<string, object>, Task>;
+	public class PasswordAuthenticationMiddleware {
+		private AppFunc next;
+
+		public void Initialize(AppFunc next) {
+			this.next = next;
+		}
+
+		public async Task Invoke(IDictionary<string, object> environment) {
+			Console.WriteLine("Begin Request");
+			await next.Invoke(environment);
+			Console.WriteLine("End Request");
+		}
+	}
+}
